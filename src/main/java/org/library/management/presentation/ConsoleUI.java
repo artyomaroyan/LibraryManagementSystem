5a40d62158787388fa380f950dc15162f5ed776e
Artyom <aroyanart98@gmail.com>
2025-10-25 00:07:59 +0400

    refactor(presentation): improve `ConsoleUI` class.
    - add small helper methods.
    - break down large methods to small methods.

--- src/main/java/org/library/management/presentation/ConsoleUI.java
+++ src/main/java/org/library/management/presentation/ConsoleUI.java
@@ -3,7 +3,13 @@ package org.library.management.presentation;
 import org.library.management.application.BookService;
 import org.library.management.application.LoanService;
 import org.library.management.application.MemberService;
+import org.library.management.model.Book;
+import org.library.management.model.Loan;
+import org.library.management.model.Member;
 
+import java.time.Year;
+import java.util.Collection;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.UUID;
 
@@ -48,92 +54,154 @@ public class ConsoleUI {
 
     private void manageBooks() {
         while (true) {
-            IO.println("\\n--- Books ---");
+            IO.println("\n--- Books Management ---");
             IO.println("1. Add new book");
-            IO.println("2. List of books");
+            IO.println("2. List all books");
             IO.println("3. Find book by title");
             IO.println("4. Delete book by ID");
-            IO.println("0. Back");
-            IO.print("Enter your choice: ");
+            IO.println("0. Back to main menu");
+            IO.print("Enter your choose: ");
 
             String choice = scanner.nextLine();
 
             switch (choice) {
-                case "1" -> {
-                    IO.print("Title: ");
-                    String title = scanner.nextLine();
-                    IO.print("Author: ");
-                    String author = scanner.nextLine();
-                    IO.print("Year: ");
-                    int year = Integer.parseInt(scanner.nextLine());
-                    IO.print("Total copies: ");
-                    int totalCopies = Integer.parseInt(scanner.nextLine());
-                    String result = bookService.addBook(title, author, year, totalCopies);
-                    IO.println(result);
-                }
-                case "2" -> bookService.listOfBooks().forEach(book -> IO.println(book.toString()));
-                case "3" -> {
-                    IO.print("Search by title: ");
-                    String title = scanner.nextLine();
-                    bookService.findBookByTitle(title).ifPresentOrElse(
-                            book -> IO.println(book.toString()),
-                            () -> IO.println("Book not found"));
-                }
-                case "4" -> {
-                    IO.print("Delete by ID: ");
-                    UUID id = UUID.fromString(scanner.nextLine());
-                    boolean deleted = bookService.deleteBookById(id);
-                    IO.println(deleted ? "Deleted" : "Not found / could not be delete");
-                }
+                case "1" -> addBook();
+                case "2" -> listBooks();
+                case "3" -> findByTitle();
+                case "4" -> deleteBook();
                 case "0" -> {
                     return;
                 }
-                default -> IO.println("Invalid");
+                default -> IO.println("Invalid choice. Please try again");
             }
         }
     }
 
+    private void addBook() {
+        try {
+            String title = readNonEmptyString("Title: ");
+            String author = readNonEmptyString("Author: ");
+            int year = readInt("Publication year: ");
+            int totalCopies = readInt("Total copies: ");
+
+            if (year < 0 || year > Year.now().getValue()) {
+                IO.println("Invalid publication year.");
+                return;
+            }
+
+            if (totalCopies <= 0) {
+                IO.println("Total copies must be positive");
+                return;
+            }
+
+            String result = bookService.addBook(title, author, year, totalCopies);
+            IO.println("Added: " + result);
+        } catch (Exception ex) {
+            IO.println("Error adding book: " + ex.getMessage());
+        }
+    }
+
+    private void listBooks() {
+        Collection<Book> books = bookService.listOfBooks();
+        if (books.isEmpty()) {
+            IO.println("No books found in the library.");
+        } else {
+            IO.println("\n--- All books ---");
+            books.forEach(book -> IO.println(book.toString()));
+        }
+    }
+
+    private void findByTitle() {
+        String title = readNonEmptyString("Enter book title to search: ");
+        bookService.findBookByTitle(title).ifPresentOrElse(book -> {
+                    IO.println("Book found:");
+                    IO.println(book.toString());
+                },
+                () -> IO.println("Book not found: "));
+    }
+
+    private void deleteBook() {
+        IO.println("Current book: ");
+        bookService.listOfBooks().forEach(book ->
+                IO.println("ID: " + book.id() + " - " + book.title()));
+        UUID id = readUUID("Enter book ID to delete: ");
+        if (id == null) return;
+
+        boolean deleted = bookService.deleteBookById(id);
+        IO.println(deleted ? "Book deleted successfully" : "Book not found or could not be deleted");
+    }
+
     private void manageMembers() {
         while (true) {
-            IO.println("\n--- Members ---");
+            IO.println("\n--- Members Management ---");
             IO.println("1. Add new member");
             IO.println("2. List of members");
             IO.println("3. Find member by name");
             IO.println("4. Delete member by ID");
-            IO.println("0. Exit");
+            IO.println("0. Back to main menu");
             IO.print("Enter your choice: ");
 
             String choice = scanner.nextLine();
 
             switch (choice) {
-                case "1" -> {
-                    IO.print("Name: ");
-                    String name = scanner.nextLine();
-                    IO.print("Email: ");
-                    String email = scanner.nextLine();
-                    String result = memberService.addMember(name, email);
-                    IO.println(result);
-                }
-                case "2" -> memberService.listOfMembers().forEach(member -> IO.println(member.toString()));
-                case "3" -> {
-                    IO.print("Search by name: ");
-                    String name = scanner.nextLine();
-                    memberService.findMemberByName(name).ifPresentOrElse(
-                            member -> IO.println(member.toString()),
-                            () -> IO.println("Member not found"));
-                }
-                case "4" -> {
-                    IO.print("Delete by ID: ");
-                    UUID id = UUID.fromString(scanner.nextLine());
-                    boolean deleted = memberService.deleteMemberById(id);
-                    IO.println(deleted ? "Deleted" : "Not found / could not be delete");
-                }
+                case "1" -> addMember();
+                case "2" -> listMembers();
+                case "3" -> findByName();
+                case "4" -> deleteMember();
                 case "0" -> {
                     return;
                 }
-                default -> IO.println("Invalid");
+                default -> IO.println("Invalid choice. Please try again");
             }
         }
+    }
+
+    private void addMember() {
+        try {
+            String name = readNonEmptyString("Name: ");
+            String email = readNonEmptyString("Email: ");
+
+            memberService.findMemberByName(name).ifPresent(m -> {
+                if (Objects.equals(m.name(), name) || Objects.equals(m.email(), email)) {
+                    IO.println("You already have an account: ");
+                }
+            });
+
+            String result = memberService.addMember(name, email);
+            IO.println("Added: " + result);
+        } catch (Exception ex) {
+            IO.println("Error adding member: " + ex.getMessage());
+        }
+    }
+
+    private void listMembers() {
+        Collection<Member> members = memberService.listOfMembers();
+        if (members.isEmpty()) {
+            IO.println("No members found. ");
+        } else {
+            IO.println("\n--- All members ---");
+            members.forEach(member -> IO.println(member.toString()));
+        }
+    }
+
+    private void findByName() {
+        String name = readNonEmptyString("Enter member name to search: ");
+        memberService.findMemberByName(name).ifPresentOrElse(member -> {
+                    IO.println("Member found: ");
+                    IO.println(member.toString());
+                },
+                () -> IO.println("Member not found: "));
+    }
+
+    private void deleteMember() {
+        IO.println("Current member: ");
+        memberService.listOfMembers().forEach(member ->
+                IO.println("ID: " + member.id() + " - " + member.name()));
+        UUID id = readUUID("Enter member ID to delete: ");
+        if (id == null) return;
+
+        boolean deleted = bookService.deleteBookById(id);
+        IO.println(deleted ? "Member deleted successfully" : "Member not found or could not be deleted");
     }
 
     private void manageLoans() {
@@ -142,32 +210,98 @@ public class ConsoleUI {
             IO.println("1. Crate new loan");
             IO.println("2. Return loan");
             IO.println("3. Check overdue loans");
-            IO.println("0. Exit");
+            IO.println("0. Back to main menu");
             IO.print("Enter your choice: ");
 
             String choice = scanner.nextLine();
 
             switch (choice) {
-                case "1" -> {
-                    IO.print("Member ID");
-                    UUID memberId = UUID.fromString(scanner.nextLine());
-                    IO.print("Book ID");
-                    UUID bookId = UUID.fromString(scanner.nextLine());
-                    boolean result = loanService.createLoan(memberId, bookId);
-                    IO.println(result ? "Loan created" : "Failed to create loan");
-                }
-                case "2" -> {
-                    IO.print("Loan ID");
-                    UUID loanId = UUID.fromString(scanner.nextLine());
-                    boolean result = loanService.returnLoan(loanId);
-                    IO.println(result ? "Loan successfully returned" : "Failed to return loan");
-                }
-                case "3" -> loanService.getOverdueLoans().forEach(loan -> IO.println(loan.toString()));
+                case "1" -> createLoan();
+                case "2" -> returnLoan();
+                case "3" -> overdueLoans();
                 case "0" -> {
                     return;
                 }
-                default -> IO.println("Invalid");
+                default -> IO.println("Invalid choice. Please try again");
             }
+        }
+    }
+
+    private void createLoan() {
+        try {
+            UUID bookId = readUUID("Book ID:");
+            UUID memberId = readUUID("Member ID: ");
+
+            if (!bookService.existsById(bookId) || !memberService.existsById(memberId)) {
+                IO.println("Error creating loan. Book or member does not exist. ");
+            } else {
+                boolean result = loanService.createLoan(memberId, bookId);
+                IO.println(result ? "Loan created" : "Failed to create loan");
+            }
+        } catch (Exception ex) {
+            IO.println("Error creating loan: " + ex.getMessage());
+        }
+    }
+
+    private void returnLoan() {
+        try {
+            UUID loanId = readUUID("LoanId: ");
+
+            if (!loanService.existsById(loanId)) {
+                IO.println("Loan does not found. Please try again. ");
+            } else {
+                boolean result = loanService.returnLoan(loanId);
+                IO.println(result ? "Loan returned" : "Failed to return laon");
+            }
+        } catch (Exception ex) {
+            IO.println("Failed to return loan: " + ex.getMessage());
+        }
+    }
+
+    private void overdueLoans() {
+        Collection<Loan> loans = loanService.getOverdueLoans();
+        if (loans.isEmpty()) {
+            IO.println("No overdue loans found. ");
+        } else {
+            IO.println("\n--- All overdue loans ---");
+            loans.forEach(loan -> IO.println(loan.toString()));
+        }
+    }
+
+    private int readInt(String str) {
+        while (true) {
+            try {
+                IO.print(str);
+                return Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException ex) {
+                IO.println("Invalid number: Please try again.");
+            }
+        }
+    }
+
+    private UUID readUUID(String str) {
+        while (true) {
+            try {
+                IO.print(str);
+                String input = scanner.nextLine().trim();
+                if (input.isEmpty()) {
+                    return null;
+                }
+                return UUID.fromString(input);
+            } catch (IllegalArgumentException ex) {
+                IO.println("Invalid UUID format. Please try again.");
+            }
+        }
+    }
+
+    private String readNonEmptyString(String str) {
+        while (true) {
+            IO.print(str);
+            String input = scanner.nextLine().trim();
+            if (!input.isEmpty()) {
+                return input;
+            }
+            IO.println("This filed can not be empty. Please try again.");
         }
     }
 }
