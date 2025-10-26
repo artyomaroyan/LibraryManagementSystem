3148433a751f3bade3e64b59f2d9cb591bf4e022
Artyom <aroyanart98@gmail.com>
2025-10-26 22:31:50 +0400

    feat. add `LoanPanel` class.

---
+++ src/main/java/org/library/management/presentation/panel/LoanPanel.java
@@ -0,0 +1,11 @@
+package org.library.management.presentation.panel;
+
+import javax.swing.*;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 26.10.25
+ * Time: 22:31:38
+ */
+public class LoanPanel extends JPanel {
+}
