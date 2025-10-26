35dbc495dd0e6a65c2739f39f4030b6a939cd20d
Artyom <aroyanart98@gmail.com>
2025-10-26 22:31:28 +0400

    feat. add `MemberPanel` class.

---
+++ src/main/java/org/library/management/presentation/panel/MemberPanel.java
@@ -0,0 +1,11 @@
+package org.library.management.presentation.panel;
+
+import javax.swing.*;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 26.10.25
+ * Time: 22:31:10
+ */
+public class MemberPanel extends JPanel {
+}
