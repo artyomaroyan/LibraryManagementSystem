180a4271fefe9232668f792c4c9cc925b63031de
Artyom <aroyanart98@gmail.com>
2025-10-26 22:30:53 +0400

    feat. add `BookPanel` class.

---
+++ src/main/java/org/library/management/presentation/panel/BookPanel.java
@@ -0,0 +1,11 @@
+package org.library.management.presentation.panel;
+
+import javax.swing.*;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 26.10.25
+ * Time: 22:30:34
+ */
+public class BookPanel extends JPanel {
+}
